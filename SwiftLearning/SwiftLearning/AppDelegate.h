85beb9f87e1a0640dff3c68a6d8a679db6d0bcee
陈超 <ccerdong@sina.cn>
2016-03-03 13:53:34 +0800

    init

---
+++ SwiftLearning/SwiftLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftLearning
+//
+//  Created by chenchao on 16/3/3.
+//  Copyright © 2016年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwiftLearning/SwiftLearning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftLearning
+//
+//  Created by chenchao on 16/3/3.
+//  Copyright © 2016年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
