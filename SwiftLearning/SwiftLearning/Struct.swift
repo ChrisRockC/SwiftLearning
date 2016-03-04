8dee9975ef790976734bb56470eb277a246642d4
陈超 <ccerdong@sina.cn>
2016-03-04 10:36:55 +0800

    结构体基本技术  Swift 就是类似一个类了  方法 属性 静态 都有了  对比学习java 学习下

--- SwiftLearning/SwiftLearning/Struct.swift
+++ SwiftLearning/SwiftLearning/Struct.swift
@@ -38,6 +38,36 @@ class Struct: NSObject {
         }
         
         //2. swift Struct内可以创建init方法
+        init(x newX:Float, y newY:Float, width newWidth:Float, height newHeight:Float){
+            //x  为外部参数名
+            //newX 为内部参数名
+//            一个比较长的有描述意义的外部参数名，
+//            在函数内部简写为短的内部参数名。
+            
+            self.x = newX
+            self.y = newY
+            self.width = newWidth
+            self.height = newHeight
+            
+        }
+        
+        //3. Swift结构体内部可以任意创建函数
+        // 如果要在函数里面修改属性的值得话，在函数前面加上mutating关键字修饰
+        mutating func changeX(){
+            self.x = 100
+        }
+        
+        //4. static属性不能在普通的函数里面调用
+        static var name:String? = nil   // 另外一种初始化 var name = String()
+        
+        //5. static函数  相当于OC的类方法 不能调用普通属性 但是可以调用static属性 ，可以调用本结构存在的一些函数？
+        static func sayName(){
+            self.name = "chenchao"
+            print(self.name)
+        }
+        
+        
+        
     }
 
 }
---
+++ SwiftLearning/SwiftLearning/SwiftViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftViewController.swift
+//  SwiftLearning
+//
+//  Created by chenchao on 16/3/4.
+//  Copyright © 2016年 iOS. All rights reserved.
+//
+
+import UIKit
+
+class SwiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //创建项目的时候用oc 用的不爽。
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
