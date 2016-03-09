395593f6c49bcb7a6c8f462cbd35ec4436907880
陈超 <ccerdong@sina.cn>
2016-03-09 16:18:28 +0800

    闭包模块

---
+++ SwiftLearning/SwiftLearning/CC-闭包.swift
@@ -0,0 +1,21 @@
+//
+//  CC-闭包.swift
+//  SwiftLearning
+//
+//  Created by chenchao on 16/3/9.
+//  Copyright © 2016年 iOS. All rights reserved.
+//
+
+import Foundation
+
+//class bibao : NSObject{
+//    // 闭包表达式五种表达式
+//    //Apple 推荐是第二种写法
+//
+//    //定义闭包
+//    var newPassValue :((a:Int,b:Int)->Int)
+//    
+//    
+//
+//}
+
